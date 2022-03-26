cf2a08726f3c1839b20010379ee5f743451b0405
Joe Susnick <joesusnick@gmail.com>
2022-03-26 17:10:59 -0600

    Initial Commit

---
+++ HanabiTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HanabiTracker
+//
+//  Created by Joe Susnick on 3/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HanabiTracker/HanabiTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanabiTrackerApp.swift
+//  HanabiTracker
+//
+//  Created by Joe Susnick on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct HanabiTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
